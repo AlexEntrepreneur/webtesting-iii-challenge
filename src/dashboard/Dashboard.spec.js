849f287f0ab3873eaaa31f6b4c35dbf1d82fbd8d
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-10 15:44:23 +0100

    Add Dashboard Tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
-// Test away
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+
+describe('Dashboard', () => {
+  it('should render controls component', () => {
+    const wrapper = rtl.render(<Dashboard />);
+    const controlsComponent = wrapper.container.querySelector('.controls.panel');
+    expect(controlsComponent).toBeInTheDocument();
+  });
+
+  it('should render display component', () => {
+    const wrapper = rtl.render(<Dashboard />);
+    const controlsComponent = wrapper.container.querySelector('.display.panel');
+    expect(controlsComponent).toBeInTheDocument();
+  });
+});
